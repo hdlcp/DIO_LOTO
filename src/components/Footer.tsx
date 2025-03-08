41daa6e8201e29deb692f953ea47a7499ed97a0a
oloukpede prosper <oloukpedeprosper@gmail.com>
2025-03-08 17:33:46 +0100

    dev hdlp

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,8 @@ import React from "react";
 const Footer: React.FC = () => {
   return (
     <footer className="footer">
-    <p>© 2024. All Rights Reserved</p>
+    <p>© 2025. All Rights Reserved</p>
+    <p>@Dev hdlp</p>
   </footer>
   
   );
