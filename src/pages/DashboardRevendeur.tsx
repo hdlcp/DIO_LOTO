361b2ccaf119a20d4daa308e780172de827dd594
VOGLOSSOU <n969601@gmail.com>
2026-01-02 14:17:36 +0100

    so good, les bonus sont stopés pour l' instant

--- src/pages/DashboardRevendeur.tsx
+++ src/pages/DashboardRevendeur.tsx
@@ -71,6 +71,7 @@ const DashboardRevendeur: React.FC = () => {
             <h3>🎯 Gains</h3>
             <p>{gain?.toLocaleString() || 0} XOF</p>
           </motion.div>
+          {/* Section bonus - Journée bonus terminée le 04/01/2026
           <motion.div
             className="balance-box bonus-balance"
             initial={{ scale: 0.8, opacity: 0 }}
@@ -83,6 +84,7 @@ const DashboardRevendeur: React.FC = () => {
               Valable le 04/01/2026 de 00h00 à 23h59
             </small>
           </motion.div>
+          */}
         </div>
 
         {/* Afficher d'autres informations spécifiques au revendeur si nécessaire */}
